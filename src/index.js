f68b95cd6385e68d37776612f102a1c719c83a7a
Rick Carlino <rick.carlino@gmail.com>
2019-05-18 19:10:42 -0500

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,47 +1,42 @@
-/*
-easy-ssb-pub: an easy way to deploy a Secure Scuttlebutt Pub.
+// === TEMP SENSOR SETUP ===
 
-Copyright (C) 2017 Andre 'Staltz' Medeiros (staltz.com)
+const sensor = require("node-dht-sensor").promises;
+const sensorType = 22;
+const sensorPin = 18;
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-var scuttlebot_1 = require("./scuttlebot");
-var ssbBot = scuttlebot_1.setupScuttlebot().ssbBot;
-
-getWeather = require("./weather");
-
-function doit() {
-  console.log("Getting weather");
-
-  getWeather()
-    .then(function (report) {
-      ssbBot.publish({
-        type: 'post',
-        text: `${report[0]}% relative humidity. Heat index is ${report[1]} deg F`
-      }, function () {
-        console.log("Done...");
-      });
-    }).catch(function (error) {
-      console.log("OH NOES");
-      console.dir(error);
-    });
-
+if (process.env.SKIP_DHT_STUFF) {
+  console.warn("NOT AN RPI; SIMULATING DHT.");
+  sensor.initialize({
+    test: {
+      fake: {
+        temperature: 21,
+        humidity: 60
+      }
+    }
+  });
 }
-var ONE_HOUR = 1000 * 60 * 60;
 
-setInterval(doit, ONE_HOUR);
+function createTemperaturePost() {
+  const res = sensor.readSync(sensorType, sensorPin);
+  const text = [
+    "Temperature (F):",
+    res.temperature.toFixed(1) * 9 / 5 + 32,
+    "Humidity:",
+    res.humidity.toFixed(1)
+  ].join(" ");
+  return { type: 'post', text }
+}
 
-doit();
+// == SERVER SETUP ===
 
+const pull = require('pull-stream')
+const Client = require('ssb-client')
+
+Client(function (err, server) {
+  if (err) throw err
+  server.publish(createTemperaturePost(), function (err, msg) {
+    if (err) throw err
+    console.dir(msg);
+    console.log("Just published a message and not doing anything else");
+  });
+});
